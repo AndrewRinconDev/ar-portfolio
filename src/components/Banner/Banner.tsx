637596722704cbdd8829cee96f99fda9072d9943
Andrés Rincón <andrew.rincon.94@gmail.com>
2024-01-31 23:45:47 -0500

    feat(main): Modify dark icons style

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -43,7 +43,7 @@ export default function Banner() {
           <li key={profile.network}>
             <Link
               href={profile.url}
-              className="rounded-full text-dark-800 dark:text-dark-200 border border-dark-300 dark:border-white/10 flex justify-center items-center gap-x-2 py-1 px-2 md:py-2 text-xs md:text-base md:px-4 bg-gradient-to-r from-slate-50 to-slate-100  dark:bg-gradient-to-r dark:from-slate-50 dark:to-slate-100 hover:scale-105 transition-transform"
+              className="rounded-full text-dark-800 dark:text-dark-200 border border-dark-300 dark:border-white/10 flex justify-center items-center gap-x-2 py-1 px-2 md:py-2 text-xs md:text-base md:px-4 bg-gradient-to-r from-slate-50 to-slate-100 dark:from-slate-800 dark:to-slate-950 hover:scale-105 transition-transform"
               target="_blank"
               rel="noopener noreferrer"
             >
