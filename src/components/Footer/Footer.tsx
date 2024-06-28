498034d9a9352a21a1030c64478f5c4033b55062
Andrés Felipe Rincón <arincon@premier.pr>
2024-06-28 18:58:36 -0500

    Update stack

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="bg-black w-full py-2">
+        <footer className="bg-black w-full py-3">
             <p className="w-full text-white text-center">
                 Copyright &copy; {new Date().getFullYear()} Andrés Rincón. All Rights reserved.
             </p>
--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -13,16 +13,16 @@ function Stack() {
 
     return (
         <div className="stack-section w-full relative">
-            <div className="w-full sm:w-[90%] lg:max-w-[850px] xl:max-w-[1000px] py-12 md:pt-20 mx-auto px-4 md:px-0">
+            <div className="w-full sm:w-[90%] lg:max-w-[850px] xl:max-w-[1000px] py-12 md:pt-16 mx-auto px-4 md:px-0">
                 <h2 className="text-3xl font-semibold mb-6 flex gap-x-3 items-center text-black dark:text-white">
                     <Icon
                         name="stack"
                         className="size-10 text-black dark:invert"
-                        color="#1ea1e8"
+                        color="#93c5fd "
                         width={36}
                         height={36}
                     />
-                    <span className="text-[#1ea1e8]">{t("stack_label")}</span>
+                    <span className="text-blue-300">{t("stack_label")}</span>
                 </h2>
                 <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-8">
                     {stack.map((stackElement, index) => (
@@ -30,7 +30,7 @@ function Stack() {
                             className="flex flex-col stack-card rounded-sm px-8 py-6 gap-6 transition-all duration-300 cursor-pointer hover:shadow-gray-400"
                             key={`stack-${stackElement.category_key}-${index}`}
                         >
-                            <h3 className="text-2xl font-semibold text-sky-300/80 text-center">
+                            <h3 className="text-2xl font-semibold text-blue-300 text-center">
                                 {t(stackElement.category_key)}
                             </h3>
                             <div className="flex justify-center gap-6 align-middle flex-wrap">
